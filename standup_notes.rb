49ed2053544b72b97c5a226ddfe928649934c82e
ladybug010 <devon@agoragames.com>
2011-05-20 16:53:28 -0400

    updating standup notes with hash

--- standup_notes.rb
+++ standup_notes.rb
@@ -1,57 +1,30 @@
-
+def ask (question)
+  puts question
+  gets.chomp
+end
  
 def take_notes
-   more_notes = true
-   #when the program is started, more notes will always be true.
-   
-  while (more_notes != false)
+  
+  standup_notes_hash = {}
+  
+  while true
     puts 'Do you have a team member to enter stand up notes for?'
     notes = gets.chomp
         
       if notes == 'no'
         puts 'thank you, please check the cooresponding file to retreieve today\'s notes.'
-        more_notes = false
+        break
         
       elsif notes == 'yes'
-        def standup (question)
-          puts question
-          reply = gets.chomp
-          return reply
-        end
-        
-        name =  standup 'Team Member\'s Name?'
-        yesterday = standup 'What did ' + name + ' do yesterday?'
-        today = standup 'What is ' + name + ' doing today?'
-        blockers = standup 'Does ' + name + ' have any blockers?'
-        puts
-        puts
-        return name
-        return yesterday
-        return today
-        return blockers
-        puts
-        
-        #now I want to make a new array, for each person. Maybe this should be a hash?
-        def make_array
-          Array.new = name _ '_array'
-        end
-        
-        make_array
-        
-        #after making that person's array, i want to push in the information from these notes, so that I can save the data for each person.
-
-        name + '_array'.push name
-        name + '_array'.push yesterday
-        name + '_array'.push today
-        name + '_array'.push blockers
+        name = ask 'Team Member\'s Name:'
+        standup_notes_hash[name]= {}
+        standup_notes_hash[name][:yesterday] = ask "What did #{name} do yesterday?"
+        standup_notes_hash[name][:today]  = ask "What is #{name} doing today?"
+        standup_notes_hash[name][:blockers]  = ask "Does #{name} have any blockers?"
       
-        
       end
   end
+  puts standup_notes_hash
 end
 
-
 take_notes
-
-#need to output the information in the arrays to a text file, saved with that days date. The file name and location path should be printed to screen at the end.
-
